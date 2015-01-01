58d06d26c0da8cae5198593a3838ba43be0d9e60
timwoj <timwoj@ieee.org>
2015-01-01 16:46:44 -0700

    Reordered columns in grid view so avg ilvls are further left

--- resources/tables.js
+++ resources/tables.js
@@ -4,6 +4,6 @@ $(document).ready(function() {
         sortForce: [[2,0]],
         // default sort order is by group (to fix the above force sorting), then avg equipped
         // (descending) and finally name (ascending)
-        sortList: [[2,0],[19,1],[0,0]],
+        sortList: [[2,0],[3,1],[0,0]],
     });
 });
