d2536a1f09a8e753f14647e7b4f8c15f2e9c726f
Tim Wojtulewicz <timwoj@gmail.com>
2019-03-25 17:09:37 -0700

    Reindent javascript

--- static/editor.js
+++ static/editor.js
@@ -5,162 +5,162 @@ var realmToNRealm = {};
 var nrealmToRealm = {};
 
 function Delete(event){
-    // event.preventDefault();
-    // event.stopPropagation();
-    
-    // TODO: confirmation box appears n-1 times where n is the number of toons
-    // on the page.  no idea why it does that.
-    let par = $(this).parent().parent(); // tr
-    // // the jquery noob in me says there *has* to be a different way to do this
-    // let toon = par.children().first().children().first().val();
-    // let r = confirm("Delete toon '"+toon+"'?");
-    // if (r == true)
-    // {
-        par.remove();
-        return true;
-    // }
-    // return r;
+  // event.preventDefault();
+  // event.stopPropagation();
+
+  // TODO: confirmation box appears n-1 times where n is the number of toons
+  // on the page.  no idea why it does that.
+  let par = $(this).parent().parent(); // tr
+  // // the jquery noob in me says there *has* to be a different way to do this
+  // let toon = par.children().first().children().first().val();
+  // let r = confirm("Delete toon '"+toon+"'?");
+  // if (r == true)
+  // {
+  par.remove();
+  return true;
+  // }
+  // return r;
 };
 
 function Add(){
-    $("#membersTable tbody").append(
-	"<tr style='font-size:14px;padding:2px 5px;text-align:center'>"+
-            "<td><input type='text' accept-charset='UTF-8' style='text-transform: none;'/></td>"+
-            "<td><select>"+
-            "<option value='tank'>Tank</option>"+
-            "<option value='healer'>Healer</option>"+
-            "<option value='dps' selected>Melee DPS</option>"+
-            "<option value='ranged'>Ranged DPS</option>"+
-            "</select></td>"+
-            "<td><select>"+
-            "<option value='main'>Main</option>"+
-            "<option value='bench'>Bench</option>"+
-            "<option value='alt'>Alt</option>"+
-            "</select></td>"+
-            "<td><div class='changeRealm'>"+nrealmToRealm[nrealm]+"</div></td>"+
-	    "<td><img src='/resources/delete.png' height='24' width='24' class='btnDelete'/></td>"+
-	    "</tr>");
+  $("#membersTable tbody").append(
+    "<tr style='font-size:14px;padding:2px 5px;text-align:center'>"+
+      "<td><input type='text' accept-charset='UTF-8' style='text-transform: none;'/></td>"+
+      "<td><select>"+
+      "<option value='tank'>Tank</option>"+
+      "<option value='healer'>Healer</option>"+
+      "<option value='dps' selected>Melee DPS</option>"+
+      "<option value='ranged'>Ranged DPS</option>"+
+      "</select></td>"+
+      "<td><select>"+
+      "<option value='main'>Main</option>"+
+      "<option value='bench'>Bench</option>"+
+      "<option value='alt'>Alt</option>"+
+      "</select></td>"+
+      "<td><div class='changeRealm'>"+nrealmToRealm[nrealm]+"</div></td>"+
+      "<td><img src='/resources/delete.png' height='24' width='24' class='btnDelete'/></td>"+
+      "</tr>");
 
-    // Recreate these bindings to include the fields from the new row
-    $(".btnDelete").bind("click", Delete);
-    $(".changeRealm").bind("click", changeRealm);
+  // Recreate these bindings to include the fields from the new row
+  $(".btnDelete").bind("click", Delete);
+  $(".changeRealm").bind("click", changeRealm);
 };
 
 var lastClicked = null;
 function changeRealm(e) {
-    $("#realmWindow").jqxWindow("open");
-    lastClicked = e.currentTarget;
+  $("#realmWindow").jqxWindow("open");
+  lastClicked = e.currentTarget;
 };
 
 function authPw() {
-    // validate the password before doing anything else
-    let pw=$("#pw").val();
-    let data='group='+ngroup+'&realm='+nrealm+'&pw='+pw;
+  // validate the password before doing anything else
+  let pw=$("#pw").val();
+  let data='group='+ngroup+'&realm='+nrealm+'&pw='+pw;
 
-    $.post('/val', data)
-        .done(function() {
-            console.log('password authentication success');
-            postdata();
-        })
-        .fail(function() {
-            console.log('password authentication failed');
-            $("#pwfail").jqxNotification("open");
-        });
-    return false;
+  $.post('/val', data)
+    .done(function() {
+      console.log('password authentication success');
+      postdata();
+    })
+    .fail(function() {
+      console.log('password authentication failed');
+      $("#pwfail").jqxNotification("open");
+    });
+  return false;
 }
 
 function postdata() {
-    let url = '/'+nrealm+'/'+ngroup;
+  let url = '/'+nrealm+'/'+ngroup;
 
-    let groupname = $('#group').val();
-    let pw = $('#pw').val();
-    let json = buildjson();
-    
-    let data = 'group='+groupname+'&json='+json+'&pw='+pw;
+  let groupname = $('#group').val();
+  let pw = $('#pw').val();
+  let json = buildjson();
 
-    $.post(url, data)
-        .done(function() {
-            window.location.replace(url);
-        })
-        .fail(function() {
-            console.log('setting group data failed');
-        });
+  let data = 'group='+groupname+'&json='+json+'&pw='+pw;
+
+  $.post(url, data)
+    .done(function() {
+      window.location.replace(url);
+    })
+    .fail(function() {
+      console.log('setting group data failed');
+    });
 }
 
 function buildjson() {
 
-    let data = {toons: []};
+  let data = {toons: []};
 
-    // using the DOM table implementation here because it's a bit more
-    // readable.  this could be changed to use jquery later.
-    let body = document.getElementById('tablebody');
-    let rowCount = body.rows.length;
-    for (let i=0; i<rowCount; i+=1) {
-        let row = body.rows[i];
-        let name = row.cells[0].childNodes[0].value.trim();
+  // using the DOM table implementation here because it's a bit more
+  // readable.  this could be changed to use jquery later.
+  let body = document.getElementById('tablebody');
+  let rowCount = body.rows.length;
+  for (let i=0; i<rowCount; i+=1) {
+    let row = body.rows[i];
+    let name = row.cells[0].childNodes[0].value.trim();
 
-        // if the name field is empty, just skip this toon
-        if (name.length == 0) {
-            continue;
-        }
-
-        name = name.toLowerCase();
-        if (name.charCodeAt(0) < 128) {
-            name = name.charAt(0).toUpperCase() + name.slice(1);
-        }
-
-        let realm = row.cells[3].childNodes[0].textContent;
-        realm = realmToNRealm[realm];
-
-        toon = {
-            name: name,
-            role: row.cells[1].childNodes[0].value.toLowerCase(),
-            status: row.cells[2].childNodes[0].value.toLowerCase(),
-            realm: realm
-        };
-
-        data['toons'].push(toon);
+    // if the name field is empty, just skip this toon
+    if (name.length == 0) {
+      continue;
     }
 
-    return JSON.stringify(data);
+    name = name.toLowerCase();
+    if (name.charCodeAt(0) < 128) {
+      name = name.charAt(0).toUpperCase() + name.slice(1);
+    }
+
+    let realm = row.cells[3].childNodes[0].textContent;
+    realm = realmToNRealm[realm];
+
+    toon = {
+      name: name,
+      role: row.cells[1].childNodes[0].value.toLowerCase(),
+      status: row.cells[2].childNodes[0].value.toLowerCase(),
+      realm: realm
+    };
+
+    data['toons'].push(toon);
+  }
+
+  return JSON.stringify(data);
 }
 
 function normalize_gn(groupname)
 {
-    let norm = groupname.replace("'","");
-    norm = norm.replace(" ", "-");
-    norm = norm.toLowerCase();
-    return norm;
+  let norm = groupname.replace("'","");
+  norm = norm.replace(" ", "-");
+  norm = norm.toLowerCase();
+  return norm;
 }
 
 $(function(){
-    $(".btnDelete").bind("click", Delete);
-    $("#btnAdd").bind("click", Add);
-    $(".changeRealm").bind("click", changeRealm);
-    $("#submit").bind("click", authPw);
+  $(".btnDelete").bind("click", Delete);
+  $("#btnAdd").bind("click", Add);
+  $(".changeRealm").bind("click", changeRealm);
+  $("#submit").bind("click", authPw);
 
-    // there's probably a better way to do this, but use the HTML to build a
-    // lookup table for the realm to normalized realm entries and store the
-    // current local realm entry
-    nrealm = $("#nrealm").val();
-    ngroup = $("#ngroup").val();
-    $("#realmSelect > option").each(function() {
-        nrealmToRealm[this.id] = this.value;
-        realmToNRealm[this.value] = this.id;
-    });
+  // there's probably a better way to do this, but use the HTML to build a
+  // lookup table for the realm to normalized realm entries and store the
+  // current local realm entry
+  nrealm = $("#nrealm").val();
+  ngroup = $("#ngroup").val();
+  $("#realmSelect > option").each(function() {
+    nrealmToRealm[this.id] = this.value;
+    realmToNRealm[this.value] = this.id;
+  });
 
-    $("#pwfail").jqxNotification({position: "top-right", autoClose: true,
-                                  autoCloseDelay: 4000, template: "error"});
+  $("#pwfail").jqxNotification({position: "top-right", autoClose: true,
+                                autoCloseDelay: 4000, template: "error"});
 
-    $("#realmWindow").jqxWindow({
-        maxHeight: 150, maxWidth: 280, minHeight: 30, minWidth: 250, height: 100, width: 270,
-        isModal: true, okButton: $("#realmOK"), cancelButton: $("#realmCancel"),
-        resizable: false, autoOpen: false, showCloseButton: false,
-    });
+  $("#realmWindow").jqxWindow({
+    maxHeight: 150, maxWidth: 280, minHeight: 30, minWidth: 250, height: 100, width: 270,
+    isModal: true, okButton: $("#realmOK"), cancelButton: $("#realmCancel"),
+    resizable: false, autoOpen: false, showCloseButton: false,
+  });
 
-    $("#realmWindow").on('close', function(event) {
-        if (event.args.dialogResult.OK) {
-            lastClicked.textContent = $("#realmSelect").val();
-        }
-    });
+  $("#realmWindow").on('close', function(event) {
+    if (event.args.dialogResult.OK) {
+      lastClicked.textContent = $("#realmSelect").val();
+    }
+  });
 });
--- static/grouploader.js
+++ static/grouploader.js
@@ -2,14 +2,14 @@ var nrealm = null;
 var ngroup = null;
 
 $(document).ready(function() {
-    nrealm = $("#nrealm").val();
-    ngroup = $("#ngroup").val();
+  nrealm = $("#nrealm").val();
+  ngroup = $("#ngroup").val();
 
-    $("#roster").tablesorter({
-        // set forced sort on the third column to ensure that subs are always at the bottom
-        sortForce: [[2,1]],
-        // default sort order is by group (to fix the above force sorting), then avg equipped
-        // (descending) and finally name (ascending)
-        sortList: [[2,1],[3,1],[0,0]],
-    });
+  $("#roster").tablesorter({
+    // set forced sort on the third column to ensure that subs are always at the bottom
+    sortForce: [[2,1]],
+    // default sort order is by group (to fix the above force sorting), then avg equipped
+    // (descending) and finally name (ascending)
+    sortList: [[2,1],[3,1],[0,0]],
+  });
 });
