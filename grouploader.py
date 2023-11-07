31fec515c88f129569b54e25b64ca8d103b3ee95
Tim Wojtulewicz <timwoj@gmail.com>
2023-11-06 20:07:35 -0700

    Update item levels and tier sets for DF Season 3

--- grouploader.py
+++ grouploader.py
@@ -13,9 +13,9 @@ from google.appengine.api import memcache
 from passlib.hash import sha256_crypt
 
 # Minimum ilvls and colors for the ilvl grid
-MIN_NORMAL = 415
-MIN_HEROIC = 428
-MIN_MYTHIC = 441
+MIN_NORMAL = 454
+MIN_HEROIC = 467
+MIN_MYTHIC = 480
 COLOR_LFR = '#FFB2B2'
 COLOR_NORMAL = '#FFFFB2'
 COLOR_HEROIC = '#B2FFB2'
@@ -38,7 +38,7 @@ CLASS_INDEXES = {
     'Evoker': 13
 }
 
-TIER_SETS = [1540, 1541, 1542, 1543, 1544, 1545, 1546, 1547, 1548, 1549, 1550, 1551, 1552]
+TIER_SETS = [1557, 1558, 1559, 1560, 1561, 1562, 1563, 1564, 1565, 1566, 1567, 1568, 1569]
 
 # This is used to color the table cells on the grid display based on the ilvl
 # of the item.  It gets put into the jinja environment as a filter.
